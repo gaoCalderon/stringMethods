fda315bc172f5e739b517105375f478706af7f28
gaoCalderon <gao.vang86@gmail.com>
2016-09-12 09:43:10 -0500

    what I have

---
+++ StringMethod/StringMethod/Methods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethod
+{
+    class Methods
+    {
+        
+
+        
+
+        public void averageNumbers(int[] array)
+        {
+            int size = array.Length;
+            int sum = array.Aggregate((a, b) => b + a);
+            double average = array.Average(x => sum / size);
+            Console.WriteLine(average);
+        }
+
+        //public void averageNums(string numbers)
+        //{
+        //    int size = numbers.Length;
+        //    int sum = numbers.Aggregate((a, b) => b + a);
+        //    double average = numbers.Average(x => sum / size);
+        //    Console.WriteLine(average);
+        //}
+
+
+        public string orderLettersCounted(string s)
+        {
+            char[] a = s.ToCharArray();
+            Array.Sort(a);
+            return new string(a);
+        }
+       
+
+    }
+}
--- StringMethod/StringMethod/Program.cs
+++ StringMethod/StringMethod/Program.cs
@@ -10,6 +10,17 @@ namespace StringMethod
     {
         static void Main(string[] args)
         {
+            int[] grades = { 90, 100, 82, 89, 55 };
+            //string gradNumbers = "90,100,82,89,55";
+            string letters = "dfskjdjkcek";
+            //Console.WriteLine(grades.Average());
+            Methods method = new Methods();
+            method.averageNumbers(grades);
+            //method.averageNums(gradNumbers);
+            method.orderLettersCounted(letters);
+            Console.ReadLine();
+
+           
         }
     }
 }
